cbeec3adc1b318c0488ed233228b1c744e7774ea
Dom Derrien <dominique.derrien@gmail.com>
2025-05-22 20:37:42 -0400

    restore the MogaToken contract as it was, w/o the use of its interface

--- contracts/MogaToken.sol
+++ contracts/MogaToken.sol
@@ -1,26 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// cSpell:ignore moga, mogaland
-
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import './IMogaToken.sol';
 
-/**
- * @title MogaToken
- * @dev This contract implements a capped ERC20 token with burnable functionality.
- * It allows the owner to mint tokens up to a specified cap.
- * The token is named "MogaToken" with the symbol "Moga", and is a utility token within the Mogaland ecosystem.
- * The contract is designed to be deployed on EVM-compatible blockchains.
- */
-contract MogaToken is ERC20Capped, ERC20Burnable, Ownable, IMogaToken {
-    constructor(address initialOwner, uint256 cap) ERC20('MogaToken', 'Moga') ERC20Capped(cap * (10 ** decimals())) Ownable(initialOwner) {}
+contract MogaToken is ERC20Capped, ERC20Burnable, Ownable {
+    constructor(
+        address initialOwner,
+        uint256 cap
+    )
+        ERC20("MogaToken", "Moga")
+        ERC20Capped(cap * (10 ** decimals()))
+        Ownable(initialOwner)
+    {}
 
-    // Overriding internal function to enforce implementation from ERC20Capped
-    function _update(address from, address to, uint256 value) internal virtual override(ERC20Capped, ERC20) {
+   // overriding internal function to enforce implementation from ERC20Capped
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override(ERC20Capped, ERC20) {
         super._update(from, to, value);
 
         if (from == address(0)) {
